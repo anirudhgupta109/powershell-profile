2ccc369558a744daafb7a2d2917afb81d85973e6
Anirudh Gupta <anirudhgupta109@gmail.com>
2026-02-03 04:34:04 +0530

    setup: Remove chocolatey
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- setup.ps1
+++ setup.ps1
@@ -150,17 +150,6 @@ if ((Test-Path -Path $PROFILE) -and (winget list --name "OhMyPosh" -e) -and ($fo
     Write-Warning "Setup completed with errors. Please check the error messages above."
 }
 
-# Choco install
-try {
-    Set-ExecutionPolicy Bypass -Scope Process -Force
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-    $chocoScript = (New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')
-    Invoke-Expression $chocoScript
-}
-catch {
-    Write-Error "Failed to install Chocolatey. Error: $_"
-}
-
 # Terminal Icons Install
 try {
     Install-Module -Name Terminal-Icons -Repository PSGallery -Force
